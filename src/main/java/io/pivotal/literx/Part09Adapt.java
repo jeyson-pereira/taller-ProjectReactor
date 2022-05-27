2184d679cdb34b0358ef40d3eb4ace7bb5e30057
Jeyson Alejandro Pereira <jpereiraf96@gmail.com>
2022-05-27 06:23:17 -0500

    Part09 Solved
    Adapt Flux, Mono, Observable and Single using methods like fromPublisher, toFlowable.

--- src/main/java/io/pivotal/literx/Part09Adapt.java
+++ src/main/java/io/pivotal/literx/Part09Adapt.java
@@ -19,15 +19,16 @@ package io.pivotal.literx;
 import java.util.concurrent.CompletableFuture;
 
 import io.pivotal.literx.domain.User;
+import io.reactivex.rxjava3.core.BackpressureStrategy;
 import io.reactivex.rxjava3.core.Flowable;
 import io.reactivex.rxjava3.core.Observable;
 import io.reactivex.rxjava3.core.Single;
-import org.reactivestreams.Publisher;
+//import org.reactivestreams.Publisher;
 import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
 
 /**
- * Learn how to adapt from/to RxJava 3 Observable/Single/Flowable and Java 8+ CompletableFuture.
+ * Solved how to adapt from/to RxJava 3 Observable/Single/Flowable and Java 8+ CompletableFuture.
  *
  * Mono and Flux already implements Reactive Streams interfaces so they are natively
  * Reactive Streams compliant + there are {@link Mono#from(Publisher)} and {@link Flux#from(Publisher)}
@@ -35,56 +36,60 @@ import reactor.core.publisher.Mono;
  *
  * For RxJava 3, you should not use Reactor Adapter but only RxJava 3 and Reactor Core.
  *
- * @author Sebastien Deleuze
+ * @author Jeyson Pereira
  */
+/**
+ * For new versions of RxJava use Adapter
+ * @see <a href="https://projectreactor.io/docs/adapter/snapshot/api/reactor/adapter/rxjava/RxJava3Adapter.html">RxJava3Adapter Reactor Doc</a>
+*/
 public class Part09Adapt {
 
 //========================================================================================
 
-	// TODO Adapt Flux to RxJava Flowable
+	// Adapt Flux to RxJava Flowable
 	Flowable<User> fromFluxToFlowable(Flux<User> flux) {
-		return null;
+		return Flowable.fromPublisher(flux);
 	}
 
-	// TODO Adapt RxJava Flowable to Flux
+	// Adapt RxJava Flowable to Flux
 	Flux<User> fromFlowableToFlux(Flowable<User> flowable) {
-		return null;
+		return Flux.from(flowable);
 	}
 
 //========================================================================================
 
-	// TODO Adapt Flux to RxJava Observable
+	// Adapt Flux to RxJava Observable
 	Observable<User> fromFluxToObservable(Flux<User> flux) {
-		return null;
+		return Observable.fromPublisher(flux);
 	}
 
-	// TODO Adapt RxJava Observable to Flux
+	// Adapt RxJava Observable to Flux
 	Flux<User> fromObservableToFlux(Observable<User> observable) {
-		return null;
+		return Flux.from(observable.toFlowable(BackpressureStrategy.BUFFER));
 	}
 
 //========================================================================================
 
-	// TODO Adapt Mono to RxJava Single
+	// Adapt Mono to RxJava Single
 	Single<User> fromMonoToSingle(Mono<User> mono) {
-		return null;
+		return Single.fromPublisher(mono);
 	}
 
-	// TODO Adapt RxJava Single to Mono
+	// Adapt RxJava Single to Mono
 	Mono<User> fromSingleToMono(Single<User> single) {
-		return null;
+		return Mono.from(single.toFlowable());
 	}
 
 //========================================================================================
 
-	// TODO Adapt Mono to Java 8+ CompletableFuture
+	// Adapt Mono to Java 8+ CompletableFuture
 	CompletableFuture<User> fromMonoToCompletableFuture(Mono<User> mono) {
-		return null;
+		return mono.toFuture();
 	}
 
-	// TODO Adapt Java 8+ CompletableFuture to Mono
+	// Adapt Java 8+ CompletableFuture to Mono
 	Mono<User> fromCompletableFutureToMono(CompletableFuture<User> future) {
-		return null;
+		return Mono.fromFuture(future);
 	}
 
 }
